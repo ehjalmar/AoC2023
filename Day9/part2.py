da8a41e502c218326db2baf4831131093c319f59
Erik Hjalmarsson <erik.hjalmarsson@sogeti.com>
2023-12-10 22:03:16 +0100

    Added day9 part2

---
+++ Day9/part2.py
@@ -0,0 +1,31 @@
+from collections import Counter
+
+
+file = open('Day9/input.txt', 'r')
+lines = file.read().splitlines()
+
+def finddiffs(numbers):
+    diffs = []
+    for previous, current in zip(numbers, numbers[1:]):
+        diffs.append(current-previous)
+    
+    if(len(Counter(diffs).values()) == 1): # If all divs are the same, traverse back up
+        return diffs[0]
+    
+    first_number = diffs[0]
+    next_number = first_number - finddiffs(diffs)
+    return next_number
+
+total = 0
+for line in lines:
+    string_numbers = line.split(' ')
+    numbers = [eval(i) for i in string_numbers]
+    next_number = finddiffs(numbers)
+    print(next_number)
+    print(numbers[0] - next_number)
+    total += numbers[0] - next_number
+
+print('<<<<<<<<< ' + str(total) + ' >>>>>>>>>')
+        
+    
+    
